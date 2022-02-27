17bbb087f5e85ca383d0fed57de8003a44d057cc
nabucodonosor-java <nabucodonosor.java@gmail.com>
2022-02-27 16:18:49 -0300

    fix request - test #02

--- front-web/src/utils/request.ts
+++ front-web/src/utils/request.ts
@@ -2,9 +2,14 @@ import axios from 'axios';
 import { FilterData } from '../types';
 import { formatDateToServer } from './formatters';
 
+if (process.env.REACT_APP_BACKEND_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
+}
+
 const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8081';
 
 const baseURL = BASE_URL;
+
 export const makeRequest = axios.create({
   baseURL
 });
